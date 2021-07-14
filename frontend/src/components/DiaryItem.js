07dd670f50c623e933a60c9ac7a6e4802765043b
katok <katoken629@googlemail.com>
2021-07-14 18:11:51 +0200

    testing push

--- frontend/src/components/DiaryItem.js
+++ frontend/src/components/DiaryItem.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import {Link} from "react-router-dom";
+import { api_base_url } from '../pages/Resource';
 
-var api_base_url = "http://localhost:8000"
 
 class DiaryItem extends Component {
     constructor(props){
@@ -11,9 +11,9 @@ class DiaryItem extends Component {
     render() { 
         var {photos, content} = this.props;
         return ( 
-            <div className="diary-detail">
+            <div className="diary-item">
                 <img className="diary-image" src={api_base_url+photos[0].image}></img>
-                <div>
+                <div >
                     <h3>{content}</h3>
                    
                 </div>
